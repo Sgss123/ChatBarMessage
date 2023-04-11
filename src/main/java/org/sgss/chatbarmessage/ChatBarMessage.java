7a4ae50a163518fbeb1ce70d848bfa4532782c2d
Sgss123 <103869422+Sgss123@users.noreply.github.com>
2023-04-11 22:32:04 +0800

    Initial commit

---
+++ src/main/java/org/sgss/chatbarmessage/ChatBarMessage.java
@@ -0,0 +1,18 @@
+package org.sgss.chatbarmessage;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChatBarMessage extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("聊天栏优化插件已加载！");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        System.out.println("聊天栏优化插件已卸载！");
+    }
+}
