3c73caeff0bce32c276f91065f99cebd2b45902d
Sgss123 <103869422+Sgss123@users.noreply.github.com>
2023-04-12 19:41:11 +0800

    添加小功能

--- src/main/java/org/sgss/chatbarmessage/ChatBarMessage.java
+++ src/main/java/org/sgss/chatbarmessage/ChatBarMessage.java
@@ -7,12 +7,12 @@ public final class ChatBarMessage extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        System.out.println("聊天栏优化插件已加载！");
+        System.out.println("聊天栏优化插件已加载!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        System.out.println("聊天栏优化插件已卸载！");
+        System.out.println("聊天栏优化插件已卸载!");
     }
 }
