8aa686790d28d57bc5f29e13158cc1493da9a010
Sgss123 <103869422+Sgss123@users.noreply.github.com>
2023-04-12 20:53:48 +0800

    添加聊天优化

---
+++ src/main/java/org/sgss/chatbarmessage/PlayerChat.java
@@ -0,0 +1,11 @@
+package org.sgss.chatbarmessage;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerChatEvent;
+
+public class PlayerChat implements Listener {
+    public void PlayerChat(AsyncPlayerChatEvent e){
+        
+    }
+}
