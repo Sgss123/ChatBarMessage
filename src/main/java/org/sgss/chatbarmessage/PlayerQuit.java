ce8352e9767742cebb6c5d8c3c0da8793838e2c0
Sgss123 <103869422+Sgss123@users.noreply.github.com>
2023-04-12 20:50:36 +0800

    Create PlayerQuit.java

---
+++ src/main/java/org/sgss/chatbarmessage/PlayerQuit.java
@@ -0,0 +1,15 @@
+package org.sgss.chatbarmessage;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerQuit implements Listener {
+    @EventHandler
+    public void PlayerQuit(PlayerQuitEvent e) {
+        Player p = e.getPlayer();
+        e.setQuitMessage(ChatColor.translateAlternateColorCodes('§', "§7[§f-§7] §6" + p.getName()));
+    }
+}
